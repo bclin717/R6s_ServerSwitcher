39d239b418214e71e7e4bf455d05323ccf757ca8
ds934118 <ds934118@gmail.com>
2019-06-25 18:48:31 +0800

    加入專案檔案。

---
+++ R6s_ServerSwitcher/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace R6s_ChangeServer {
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window {
+        readonly string[] areas = { "default", "eus", "cus", "scus", "wus", "sbr", "neu", "weu", "eas", "seas", "eau", "wja" };
+        string url_document;
+        string[] directories;
+
+
+        public MainWindow() {
+            InitializeComponent();
+            url_document = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\My Games\\Rainbow Six - Siege";
+
+            if (!Directory.Exists(url_document)) {
+                Console.WriteLine("Game does not exist!!");
+                this.Close();
+                Environment.Exit(Environment.ExitCode);
+            }
+
+            directories = Directory.GetDirectories(url_document);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            switchArea(ComboBox_Server.SelectedIndex);
+            MessageBox.Show("Change server successfully!");
+        }
+
+        private void switchArea(int uid) {
+            foreach (string dir in directories) {
+                string url = dir + "\\GameSettings.ini";
+                if (!checkIfFileExist(url)) { continue; }
+                editGameSettings(url, uid);
+            }
+        }
+
+        private Boolean checkIfFileExist(string url) {
+            if (!File.Exists(url)) {
+                Console.WriteLine("GameSettings.ini does not exist!!");
+                return false;
+            }
+            return true;
+        }
+
+        private void editGameSettings(string url, int uid) {
+            string[] contentInGameSetting;
+            int length = 14;
+            contentInGameSetting = File.ReadAllLines(url);
+            for (byte i = 1; i < contentInGameSetting.Length; i++) {
+
+                if (contentInGameSetting[contentInGameSetting.Length - i].Length <= 10) {
+                    continue;
+                } else {
+                    string lineString = contentInGameSetting[contentInGameSetting.Length - i].Substring(0, length);
+                    if (lineString == "DataCenterHint") {
+                        contentInGameSetting[contentInGameSetting.Length - i] = "DataCenterHint=" + areas[uid];
+                        File.WriteAllLines(url, contentInGameSetting);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Rainbow Six: Siege server switcher
+
+## Description
+
+Just start the program then chnage the server whatever you want.
